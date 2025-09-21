3e05b42a6187fa92e539c045d0127608bc9eb375
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2025-09-21 17:34:39 +0800

    ✏️ update: add user and auth route files to structure routing

---
+++ backend/src/routes/authRoutes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Validation
+// ...
+
+// Route Handlers pointed to controller functions
+// ...
+
+export default router;
---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Validation here
+// ...
+
+// Router handlers here pointing to controller functions
+// ...
+
+export default router;
+
+
