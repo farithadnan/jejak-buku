e0267c1392557ad1be4fca90b24118b1f6bf24a4
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2025-10-20 19:41:04 +0800

    feat: configure dotenv for Jest setup to use .env.test file

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,4 +1,6 @@
 const { execSync } = require('child_process');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env.test') });
 
 module.exports = async () => {
   // Run migrations for the in-memory DB
